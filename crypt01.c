605926f739713707fc3d74b667f60ebb4ab2423e
Mauricio Garduno <strip.sides.dj@comunidad.unam.mx>
2017-10-22 18:52:16 -0500

    Crypt Hash Attack en 7 lineas

--- crypt01.c
+++ crypt01.c
@@ -4,9 +4,7 @@
 int main(){
 	FILE *f=fopen ("dic.txt", "r");
 	char a[512]="$#$salt$hashpass";
-	char b[512]="$#$salt$"; 
-  char c[512]; 
-  char *d;
+	char b[512]="$#$salt$"; char c[512]; char *d;
 	
 	do {fscanf(f,"%s",c);
 		d=crypt(c,b);
